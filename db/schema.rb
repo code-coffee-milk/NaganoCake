a2355ef3d3e064321c99640d365298d5ce7cde66
sakiponjuice <saki.pon.juice0922@gmail.com>
2021-01-04 05:37:43 +0000

    ログイン機能修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210104033948) do
+ActiveRecord::Schema.define(version: 20210104053349) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
