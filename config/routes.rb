dd510ae18477b7c523d4c990984fd26d5938cd9f
sakiponjuice <saki.pon.juice0922@gmail.com>
2021-01-05 02:40:01 +0000

    [Add] ルート完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  root to: 'homes#home'
+  get 'about' => 'homes#about'
+
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :customers ,:only => [:show, :edit,:update]
-  get '/customers/unsubscribe' => 'customers#unsubscribe'
-  patch '/customers/withdraw' => 'customers#withdraw'
-  root to 'homes#home'
-  get '/about' => 'homes#about'
+  get 'customers/unsubscribe' => 'customers#unsubscribe'
+  patch 'customers/withdraw' => 'customers#withdraw'
   resources :products ,:only => [:show,:index]
   resources :cart_items ,:only => [:index, :update, :create,:destroy]
-  delete '/cart_items/destroy_all' => 'cart_items#destroy'
+  delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
   resources :orders ,:only => [:index,:show,:new,:create]
-  post '/orders/comfilm' => 'orders#comfilm'
-  get '/orders/complete' => 'orders#complete'
+  post 'orders/comfilm' => 'orders#comfilm'
+  get 'orders/complete' => 'orders#complete'
   resources :shippings, :only => [:index,:edit,:create,:update,:destroy]
-  get '/admin/homes/top', 'homes#top'
-  resources :products, only
-  
+
+  devise_for :admins
+  namespace :admin do
+  get 'admin/homes/top' => 'homes#top'
+  resources :products, :only => [:index,:show,:new,:create,:edit,:update]
+  resources :genres, :only => [:index,:create,:edit,:show,:update]
+  resources :customers, :only => [:index,:show,:edit,:update]
+  resources :orders, :only => [:index,:show,:update]
+  patch 'admin/order_details/:id' => 'order_productss#update'
+  end
 end
