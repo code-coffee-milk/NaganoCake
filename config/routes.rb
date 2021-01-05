21c69bb7852cb8bfcf74214c24bedbc12759aa9f
sakiponjuice <saki.pon.juice0922@gmail.com>
2021-01-05 13:11:48 +0000

    [Add] 新規View

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,29 @@
 Rails.application.routes.draw do
   root to: 'customers/homes#home'
   get 'home/about' => 'customers/homes#about'
+  get 'customers/unsubscribe' => 'customers/customers#unsubscribe'
+  patch 'customers/withdraw' => 'customers/customers#withdraw'
+  delete 'cart_items/destroy_all' => 'customers/cart_items#destroy_all'
+  post 'orders/comfilm' => 'customers/orders#comfilm'
+  get 'orders/complete' => 'customers/orders#complete'
+  get 'admin/homes/top' => 'admin/homes#top'
+  patch 'admin/order_details/:id' => 'admin/order_products#update'
 
   devise_for :customers
   namespace :customers do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :customers ,:only => [:show, :edit,:update]
-  get 'customers/unsubscribe' => 'customers#unsubscribe'
-  patch 'customers/withdraw' => 'customers#withdraw'
   resources :products ,:only => [:show,:index]
   resources :cart_items ,:only => [:index, :update, :create,:destroy]
-  delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
   resources :orders ,:only => [:index,:show,:new,:create]
-  post 'orders/comfilm' => 'orders#comfilm'
-  get 'orders/complete' => 'orders#complete'
   resources :shippings, :only => [:index,:edit,:create,:update,:destroy]
   end
-  
+
   devise_for :admins
   namespace :admin do
-  get 'admin/homes/top' => 'homes#top'
   resources :products, :only => [:index,:show,:new,:create,:edit,:update]
   resources :genres, :only => [:index,:create,:edit,:show,:update]
   resources :customers, :only => [:index,:show,:edit,:update]
   resources :orders, :only => [:index,:show,:update]
-  patch 'admin/order_details/:id' => 'order_productss#update'
   end
 end
