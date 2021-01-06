6e031ea5f5eb29da95d5c1c6c83b1b4146e9f6fb
EC2 Default User <ec2-user@ip-172-31-28-241.ec2.internal>
2021-01-06 09:45:49 +0000

    admin/products

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -1,19 +1,37 @@
 class Admins::ProductsController < ApplicationController
+  
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def new
+   @product = Product.new
   end
 
   def create
+    product = Product.new(product_params)
+    product.save
+    redirect_to admins_products_path
   end
 
   def edit
+   @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+    redirect_to admins_products_path(@product)
+    end
   end
+  
+    private
+  def product_params
+    params.require(:product).permit(:name, :introduction, :price, :genre_id, :image)
+  end
+  
 end
--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -1,2 +1,7 @@
 class Customers::CustomersController < ApplicationController
+    
+ def show
+
+ end
+
 end
