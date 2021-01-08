f962bfee62a4a47552bae57afd8d068fe5708ef3
EC2 Default User <ec2-user@ip-172-31-27-94.ec2.internal>
2021-01-08 09:57:28 +0000

    orders/controller now

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,10 +1,39 @@
 class Admins::OrdersController < ApplicationController
-  def index
-  end
+  
+ def index
+  @orders = Order.all
+ end
 
-  def show
-  end
+ def current_index
+  @orders = Order.where(customer_id: params[:id])
+  render :index
+ end
 
-  def update
-  end
+ def today_order_index
+  now = Time.current
+  @orders = Order.where(created_at: now.all_day)
+  render :index
+ end
+
+	def show
+		@order = Order.find(params[:id])
+		@order_products = @order.order_products
+	end
+
+	def update
+		@order = Order.find(params[:id])
+		if @order.update(order_params)
+		 flash[:success] = "注文ステータスを変更しました"
+		 redirect_to admin_order_path(@order)
+		else
+		 render "show"
+		end
+	end
+
+	private
+	def order_params
+		  params.require(:order).permit(:status)
+	end
+
 end
+
