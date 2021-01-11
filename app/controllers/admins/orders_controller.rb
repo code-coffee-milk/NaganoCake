eb9d542ca9ba69da34730e87ff2f06feb694b620
sakiponjuice <saki.pon.juice0922@gmail.com>
2021-01-11 11:21:28 +0000

    [Fix] 管理者側の注文履歴一覧画面

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,7 +1,7 @@
 class Admins::OrdersController < ApplicationController
-  
+
  def index
-  @orders = Order.all
+  @orders = Order.all.page(params[:page]).per(10)
  end
 
  def current_index
--- app/models/order.rb
+++ app/models/order.rb
@@ -14,4 +14,12 @@ class Order < ApplicationRecord
     end
     return self.shipping_fee + total_price
   end
+
+  def  total_count
+    total_count = 0
+    self.order_products.each do |product|
+      total_count += product.quantity
+    end
+    return total_count
+  end
 end
