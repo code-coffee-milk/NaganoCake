59a44061d8966d32bbd0c0098bf9356b79ae6053
sakiponjuice <saki.pon.juice0922@gmail.com>
2021-01-11 10:51:43 +0000

    [Fix] 注文履歴一覧画面

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,12 @@ class Order < ApplicationRecord
   def full_address
     return "〒" + self.shipping_postal_code + " " + self.shipping_street_adress
   end
+
+  def total_payment
+    total_price = 0
+    self.order_products.each do |product|
+      total_price += product.purchase_price * product.quantity
+    end
+    return self.shipping_fee + total_price
+  end
 end
