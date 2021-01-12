3375cc76b2567a12b85921fc99ddd588b90f50ff
sakiponjuice <saki.pon.juice0922@gmail.com>
2021-01-12 12:45:41 +0000

    [Fix] Enumで定義した部分を修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -23,7 +23,7 @@ class Order < ApplicationRecord
     return total_count
   end
 
-  enum method_of_payment: {クレジットカード: 0, 銀行振り込み: 1}
+  enum method_of_payment: {クレジットカード: 0, 銀行振込: 1}
 
   enum status: {入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4}
 
